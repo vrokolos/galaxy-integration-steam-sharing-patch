ffee263a87bd5c41876d5f36aae8856b6691f671
Iosif Karaioannoglou <iosifak@gmail.com>
2019-12-08 19:13:06 +0200

    Update pluginsharing.py
    fixes

--- pluginsharing.py
+++ pluginsharing.py
@@ -12,13 +12,11 @@ import webbrowser
 
 from typing import Any, Dict, List, Optional
 from galaxy.api.errors import AuthenticationRequired, UnknownBackendResponse
-from galaxy.api.types import Game, GameTime, LicenseInfo
+from galaxy.api.types import Achievement, Game, GameTime, LicenseInfo, GameLibrarySettings, UserPresence
 from galaxy.api.plugin import create_and_run_plugin
 from galaxy.api.consts import LicenseType
-from plugin import SteamPlugin
+from plugin import logger, SteamPlugin, is_uri_handler_installed
 from client import load_vdf, get_configuration_folder
-
-logger = logging.getLogger(__name__)
 
 class SteamSharingPlugin(SteamPlugin):
     def __init__(self, reader, writer, token):
@@ -35,8 +33,9 @@ class SteamSharingPlugin(SteamPlugin):
 
         return owned_games
 
-    async def get_steam_sharing_games(self,owngames: List[str]) -> List[Game]:
-        profiles = list(filter(lambda x: "#*" in x.user_name, self._own_friends))
+    async def get_steam_sharing_games(self, owngames: List[str]) -> List[Game]:
+        profiles = list(
+            filter(lambda x: "#*" in x.user_name, self._own_friends))
         newgames: List[Game] = []
         self._family_sharing_games = []
         for i in profiles:
@@ -44,14 +43,16 @@ class SteamSharingPlugin(SteamPlugin):
 
             try:
                 for game in othergames:
-                    hasit = any(f == str(game["appid"]) for f in owngames) or any(f.game_id == str(game["appid"]) for f in newgames)
+                    hasit = any(f == str(game["appid"]) for f in owngames) or any(
+                        f.game_id == str(game["appid"]) for f in newgames)
                     if not hasit:
                         self._family_sharing_games.append(str(game["appid"]))
                         newgame = Game(
                             str(game["appid"]),
                             game["name"],
                             [],
-                            LicenseInfo(LicenseType.OtherUserLicense, i.user_name)
+                            LicenseInfo(
+                                LicenseType.OtherUserLicense, i.user_name)
                         )
                         newgames.append(newgame)
             except (KeyError, ValueError):
@@ -61,11 +62,12 @@ class SteamSharingPlugin(SteamPlugin):
 
     async def _get_game_times_dict(self) -> Dict[str, GameTime]:
         game_times = await super()._get_game_times_dict()
-        
+
         try:
             steamFolder = get_configuration_folder()
-            vdfFile = os.path.join(steamFolder, "userdata", self._miniprofile_id, "config", "localconfig.vdf")
-            logger.debug(f"Users Localconfig.vdf {vdfFile}")
+            vdfFile = os.path.join(
+                steamFolder, "userdata", self._miniprofile_id, "config", "localconfig.vdf")
+            logging.debug(f"Users Localconfig.vdf {vdfFile}")
             data = load_vdf(vdfFile)
             timedata = data["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["Apps"]
             for gameid in self._family_sharing_games:
@@ -85,10 +87,30 @@ class SteamSharingPlugin(SteamPlugin):
 
     async def get_friends(self):
         self._own_friends = await super().get_friends()
-        return self._own_friends;
+        return self._own_friends
+
+    async def launch_game(self, game_id):
+        await super().launch_game(game_id)
+
+    async def install_game(self, game_id):
+        await super().install_game(game_id)
+
+    async def uninstall_game(self, game_id):
+        await super().uninstall_game(game_id)
+
+    async def shutdown_platform_client(self) -> None:
+        return await super().shutdown_platform_client()
+
+    async def get_user_presence(self, user_id: str, context: Any) -> UserPresence:
+        return await super().get_user_presence(user_id, context)
+        
+    async def get_local_games(self):
+        return await super().get_local_games()
+
 
 def main():
     create_and_run_plugin(SteamSharingPlugin, sys.argv)
+
 
 if __name__ == "__main__":
     main()
